6a55f9b0ded3cacb7e71529ebcb646eefb221ae2
Ismael <ismaproco@gmail.com>
2016-03-30 22:09:41 +0200

    added promises handler to the application

--- client/app.js
+++ client/app.js
@@ -1,27 +1,18 @@
 'use strict';
 
 var config = require('./config');
-var pg = require('pg');
+var qm = require('./factories/db/queryHandler');
 
-console.log('connection', config.dbConnection);
-var conString = config.dbConnection;
-//this initializes a connection pool
-//it will keep idle connections open for a (configurable) 30 seconds
-//and set a limit of 20 (also configurable)
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT keyword_id, keyword, usage from KEYWORDS order by usage', function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
+//set the config object of the queryHandler
+qm.setConfig(config);
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    
-    var row = result.rows[0];
-    console.log(row.keyword_id, row.keyword, row.usage);
-    //output: 1
-  });
+//get the query promise
+var result = qm.execute('SELECT keyword_id, keyword, usage from KEYWORDS order by usage');
+
+result
+.then( function(result){
+  console.log('then executed', result);
+})
+.fail( function(err){
+  console.log('error', err);
 });
