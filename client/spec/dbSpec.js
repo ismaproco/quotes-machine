fd337c1ac3ee746f991e76bd65010c6e8c60481a
Ismael <ismaproco@gmail.com>
2016-04-08 12:06:20 +0200

    Refactor test specs

--- client/spec/dbSpec.js
+++ client/spec/dbSpec.js
@@ -1,7 +1,6 @@
 var config = require('../config');
 var qm = require('../factories/db/queryHandler');
 var jm = require('../factories/readers/JSONHandler');
-var dm = require('../factories/db/dalManager');
 
 describe('db test', function(){
 
@@ -12,7 +11,6 @@ describe('db test', function(){
       done();  
     }
   };
-
   afterEach(() => {
     //release clients that remain open
     qm.releaseClients();  
@@ -32,39 +30,4 @@ describe('db test', function(){
     }).fail((err) => { handleFail(err,done) });
   });
 
-  it('- keyword validation', (done) => {
-    // cm = clientManager
-    qm.getClient().then( ( cm ) => {
-
-      dm.keywords.get(cm, 'XXXXX').then( (result) => {
-        expect(result.length).toBe(0);
-      }).fail((err) => { handleFail(err,done) });
-
-      dm.keywords.get(cm, 'John Green (author)').then( (result) => {
-        expect(result.length).toBeGreaterThan(0);
-        done()
-      }).fail((err) => { handleFail(err,done) });
-
-    }).fail((err) => { handleFail(err,done) });
-  });
-  
-  it('- keyword insert ', (done) => {
-    // cm = clientManager
-    qm.getClient().then( ( cm ) => {
-      dm.keywords.insert(cm, ['Gabriel Garcia Marquez', null ]).then( (result) => {
-        expect(result.length).toBeGreaterThan(0);
-        done();
-      }).fail((err) => { handleFail(err,done) });
-    }).fail((err) => { handleFail(err,done) });
-  });
-
-  it('- keyword delete ', (done) => {
-    // cm = clientManager
-    qm.getClient().then( ( cm ) => {
-      dm.keywords.remove(cm, ['Gabriel Garcia Marquez']).then( (result) => {
-        expect(result).toBeGreaterThan(0);
-        done();
-      }).fail((err) => { handleFail(err,done) });
-    }).fail((err) => { handleFail(err,done) });
-  });
 });
---
+++ client/spec/keywordsSpec.js
@@ -0,0 +1,57 @@
+var config = require('../config');
+var qm = require('../factories/db/queryHandler');
+var dm = require('../factories/db/dalManager');
+
+describe('db test', function(){
+
+  var handleFail = (err, done) => {
+    console.error('FAIL',err);
+    expect(err).toBeUndefined();
+    if(done) {
+      done();  
+    }
+  };
+
+  qm.setConfig(config);
+  
+  afterEach(() => {
+    //release clients that remain open
+    qm.releaseClients();  
+  });
+
+  it('- keyword validation', (done) => {
+    // cm = clientManager
+    qm.getClient().then( ( cm ) => {
+
+      dm.keywords.get(cm, 'XXXXX').then( (result) => {
+        expect(result.length).toBe(0);
+      }).fail((err) => { handleFail(err,done) });
+
+      dm.keywords.get(cm, 'John Green (author)').then( (result) => {
+        expect(result.length).toBeGreaterThan(0);
+        done()
+      }).fail((err) => { handleFail(err,done) });
+
+    }).fail((err) => { handleFail(err,done) });
+  });
+  
+  it('- keyword insert ', (done) => {
+    // cm = clientManager
+    qm.getClient().then( ( cm ) => {
+      dm.keywords.insert(cm, ['Gabriel Garcia Marquez', null ]).then( (result) => {
+        expect(result.length).toBeGreaterThan(0);
+        done();
+      }).fail((err) => { handleFail(err,done) });
+    }).fail((err) => { handleFail(err,done) });
+  });
+
+  it('- keyword delete ', (done) => {
+    // cm = clientManager
+    qm.getClient().then( ( cm ) => {
+      dm.keywords.remove(cm, ['Gabriel Garcia Marquez']).then( (result) => {
+        expect(result).toBeGreaterThan(0);
+        done();
+      }).fail((err) => { handleFail(err,done) });
+    }).fail((err) => { handleFail(err,done) });
+  });
+});
