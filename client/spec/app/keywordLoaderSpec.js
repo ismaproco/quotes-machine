8c2c2df6f0aa0c1cc713a1f12dce66f653e54bb4
Ismael <ismaproco@gmail.com>
2016-07-09 17:15:24 +0200

    fixed failing statements in the spec

--- client/spec/app/keywordLoaderSpec.js
+++ client/spec/app/keywordLoaderSpec.js
@@ -1,27 +1,51 @@
 var kl = require('../../factories/loaders/keywordLoader');
-
+var config = require('../../config');
 
 describe('test the keywordLoader', () => {
+    var path = config.keywordPath;
 
-    it('get total of entrances', ( done ) => {
-      kl.load().then(() => {
-        done();
-        expect( !isNaN( kl.count() ) ).toBe(true);  
-      });
+  it('get data from the Loader', ( done ) => {
+    kl.load(path).then((data) => {
+
+      expect( !!data ).toBe(true);
+      done();
+    },(err) => {
+      console.log('err', err);
+    }).fail((error) => {
+      expect(error).toBeUndefined();
+      done();
     });
+  });
 
-    it('get next 10 entrances', ( done ) => {
-      kl.getNext(10).then((result) => {
-        done();
-        expect( result.length ).toBeLowerOrEqualThan(10);  
-      });
+  it('the loader have more than 0 items', ( done ) => {
+    kl.load(path).then((data) => {
+      expect(  kl.count()  ).toBeGreaterThan(0);
+      done();
+    },(err) => {
+      console.log('err', err);
+    }).fail((error) => {
+      expect(error).toBeUndefined();
+      done();
     });
+  });
 
-    it('get all entrances', ( done ) => {
-      kl.getAll().then((result) => {
-        done();
-        expect( result.length ).toBe(kl.count());  
-      });
+  it('get next 10 entrances', ( done ) => {
+    kl.getNext(10).then((result) => {
+      expect( result.length ).not.toBeGreaterThan(10);  
+      done();
+    }).fail((error) => {
+      expect("error").toBeUndefined();
+      done();
     });
+  });
 
+  it('get all entrances', ( done ) => {
+    kl.getAll(path).then((result) => {
+      expect( result.length ).toBe(kl.count());  
+      done();
+    }).fail((error) => {
+      expect("error").toBeUndefined();
+      done();
+    });
+  });
 });
